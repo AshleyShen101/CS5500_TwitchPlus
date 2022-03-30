e142c4cb197a46f5d38b58c105d5a72d952b6725
JiangfengDong <jfdong829@gmail.com>
2022-03-30 16:36:04 -0700

    1. Update RegisterService for encrypted password 2. Update LoginService for encrypted password

--- src/main/java/com/group/jupiter/service/LoginService.java
+++ src/main/java/com/group/jupiter/service/LoginService.java
@@ -1,6 +1,7 @@
 package com.group.jupiter.service;
 
 import com.group.jupiter.dao.LoginDAO;
+import com.group.jupiter.util.Util;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +16,7 @@ public class LoginService {
     }
 
     public String verifyLogin(String userId, String password) {
+        password = Util.encryptPassword(userId, password);
         return loginDAO.verifyLogin(userId, password);
     }
 }
--- src/main/java/com/group/jupiter/service/RegisterService.java
+++ src/main/java/com/group/jupiter/service/RegisterService.java
@@ -2,6 +2,7 @@ package com.group.jupiter.service;
 
 import com.group.jupiter.dao.RegisterDAO;
 import com.group.jupiter.entity.db.User;
+import com.group.jupiter.util.Util;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,6 +17,9 @@ public class RegisterService {
     }
 
     public boolean register(User user) {
+        String userId = user.getUserId();
+        String password = user.getPassword();
+        user.setPassword(Util.encryptPassword(userId, password));
         return registerDAO.register(user);
     }
 }
