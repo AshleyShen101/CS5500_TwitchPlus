f57a30ad5e2186f254805bb80258f7db754158f8
JiangfengDong <jfdong829@gmail.com>
2022-03-30 22:00:06 -0700

    Update FavoriteDAO for Recommendation

--- src/main/java/com/group/jupiter/dao/FavoriteDAO.java
+++ src/main/java/com/group/jupiter/dao/FavoriteDAO.java
@@ -1,15 +1,14 @@
 package com.group.jupiter.dao;
 
 import com.group.jupiter.entity.db.Item;
+import com.group.jupiter.entity.db.ItemType;
 import com.group.jupiter.entity.db.User;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import java.util.Collections;
-import java.util.Objects;
-import java.util.Set;
+import java.util.*;
 
 @Repository
 public class FavoriteDAO {
@@ -69,6 +68,44 @@ public class FavoriteDAO {
             e.printStackTrace();
         }
         return Collections.emptySet();
+    }
+
+    public Set<String> getFavoriteItemIds(String userId) {
+        Set<String> itemIds = new HashSet<>();
+        try (Session session = sessionFactory.openSession()) {
+            User user = session.get(User.class, userId);
+            if (Objects.isNull(user)) {
+                return itemIds;
+            }
+
+            Set<Item> items = user.getItemSet();
+            for (Item item : items) {
+                itemIds.add(item.getId());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return itemIds;
+    }
+
+    public Map<String, List<String>> getFavoriteGameIds(Set<String> favoriteItemIds) {
+        Map<String, List<String>> itemMap = new HashMap<>();
+        for (ItemType type : ItemType.values()) {
+            itemMap.put(type.toString(), new ArrayList<>());
+        }
+
+        try (Session session = sessionFactory.openSession()) {
+            for (String itemId : favoriteItemIds) {
+                Item item = session.get(Item.class, itemId);
+                String itemType = item.getItemType().toString();
+                String gameId = item.getGameId();
+                itemMap.get(itemType).add(gameId);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return itemMap;
     }
 }
 
